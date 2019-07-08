63e5f50a8f9f60b3bc816deaba774c6e57fcf617
Pixel-0 <tiny0pixel@gmail.com>
2019-07-08 15:40:44 +0300

    edit css

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,6 +2,5 @@ export class Quote {
   public showDescription: boolean;
   constructor(public id: number, public name: string, public description: string,public publisher: string, public completeDate: Date) {
     this.showDescription = false
-
   }
 }
